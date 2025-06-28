8724c1d3f02e07b2c8a58c059e16be291c640394
Samyuktha <124591420+spsamyu@users.noreply.github.com>
2025-06-28 09:14:30 +0530

    Delete tasks directory

--- tasks/CheckOdd.java
+++
@@ -1,7 +0,0 @@
-package tasks;
-
-public class CheckOdd {
-	public static void checkNumber(int num) throws OddNumberException {
-        if (num % 2 != 0) throw new OddNumberException("Odd number not allowed");
-    }
-}
--- tasks/OddNumberException.java
+++
@@ -1,7 +0,0 @@
-package tasks;
-
-class OddNumberException extends Exception {
-    OddNumberException(String message) {
-        super(message);
-    }
-}
